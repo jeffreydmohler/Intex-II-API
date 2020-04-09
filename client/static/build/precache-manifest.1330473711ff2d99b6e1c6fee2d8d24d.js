ce76388860a015499f5f7df296feca285c739c29
Jeffrey Mohler <jeffreydmohler@outlook.com>
2020-04-09 14:10:13 -0600

    hopeuflly good

---
+++ client/static/build/precache-manifest.1330473711ff2d99b6e1c6fee2d8d24d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445ba2af191ca99585b283a514bb1f76",
+    "url": "/static/build/index.html"
+  },
+  {
+    "revision": "959c02b6274df67e5656",
+    "url": "/static/build/static/css/2.10dda66d.chunk.css"
+  },
+  {
+    "revision": "b1bd2ed069927fb98313",
+    "url": "/static/build/static/css/main.75ea473c.chunk.css"
+  },
+  {
+    "revision": "959c02b6274df67e5656",
+    "url": "/static/build/static/js/2.75f7e48e.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/build/static/js/2.75f7e48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bd2ed069927fb98313",
+    "url": "/static/build/static/js/main.8fcb853a.chunk.js"
+  },
+  {
+    "revision": "79dbfad1f01f725d21fc",
+    "url": "/static/build/static/js/runtime-main.5bda49c2.js"
+  },
+  {
+    "revision": "8d4ce80b8daa3216d1a451e29c24ee56",
+    "url": "/static/build/static/media/FootageOfSunsetView.8d4ce80b.mp4"
+  },
+  {
+    "revision": "a6ad39226b692836fff09a3e37109539",
+    "url": "/static/build/static/media/gift.a6ad3922.svg"
+  },
+  {
+    "revision": "915fd8a870100a13af1e11a6f3ea39dd",
+    "url": "/static/build/static/media/like.915fd8a8.svg"
+  },
+  {
+    "revision": "39d1f6b6e055f132fa5ab2d7034a2b08",
+    "url": "/static/build/static/media/piggyBank.39d1f6b6.svg"
+  },
+  {
+    "revision": "cb5c3e412bfee6be2967352117a625bb",
+    "url": "/static/build/static/media/world.cb5c3e41.svg"
+  }
+]);
--- intexapi/urls.py
+++ intexapi/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
 from django.conf.urls import include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-]
+    re_path(r'^.*/', include('client.urls')),
+    re_path(r'', include('client.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
