9411d613ef28e885659ab83f95c7c0672ef97c32
Jeffrey Mohler <jeffreydmohler@outlook.com>
2020-04-09 14:19:06 -0600

    hopefully this time

--- intexapi/urls.py
+++ intexapi/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf.urls import include
+from django.conf import settings
 
 
 urlpatterns = [
